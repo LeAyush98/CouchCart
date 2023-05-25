d6f36e10676c7d9562a9173491ac355f110aa788
leAyush98 <rod999lee@gmail.com>
2023-05-26 01:45:58 +0530

    added models

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Movie, Cart
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    model = Movie
+    fields = ["name","year","genre","price","duration","rating","synopsis","image"]
+
+class CartAdmin(admin.ModelAdmin):
+    model = Cart
+    fields = ["name","price","user"]
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Cart, CartAdmin)    
--- data/models.py
+++ data/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=30)
+    genre = models.CharField(max_length=30)
+    rating = models.FloatField()
+    duration = models.CharField(max_length=30)
+    year = models.IntegerField()
+    synopsis = models.CharField(max_length=500)
+    image = models.URLField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+class Cart(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    user = models.ForeignKey(to= User, on_delete=models.CASCADE)
+
+
+
